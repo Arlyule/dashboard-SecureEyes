2e3335a650e67d96bef194a28fe7d39049d5c6da
Arlyule <torresreyessamel2@gmail.com>
2024-08-27 20:50:17 -0600

    Dashboard v1

--- lib/widgets/activity_details_card.dart
+++ lib/widgets/activity_details_card.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:dashboard_secureeyes/util/responsive.dart';
 import 'package:dashboard_secureeyes/widgets/custom_card_widget.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_sound/flutter_sound.dart'; // Para grabación de audio
 import 'package:dashboard_secureeyes/main.dart'; // Importar MQTTService
 import 'package:mqtt_client/mqtt_client.dart';
 
@@ -14,75 +14,114 @@ class ActivityDetailsCard extends StatefulWidget {
 }
 
 class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
-  FlutterSoundRecorder _audioRecorder = FlutterSoundRecorder();
   bool _isRecording = false;
-  final MQTTService mqttService = MQTTService(); // Instancia de MQTTService
+  bool _isPlaying = false;
+  bool _isAlarmActive = false;
+  bool _isButtonVisible = true;
+  late MQTTService mqttService;
 
-  // Variables para almacenar los estados de los sensores y el LED
   String _luzValue = "N/A";
   String _ledValue = "N/A";
   String _microfonoValue = "N/A";
+  String reproduccion = "N/A";
   String _pirValue = "N/A";
+  Timer? _playbackTimer; // Timer para el control de reproducción
+  Timer? _alarmTimer; // Timer para el control de alarma
 
   @override
   void initState() {
     super.initState();
-    _initAudioRecorder();
+    mqttService = MQTTService();
     _setupMQTT();
   }
 
-  void _initAudioRecorder() async {
-    await _audioRecorder.openRecorder();
+  void _setupMQTT() async {
+    await mqttService.connect();
+
+    mqttService.subscribeToTopic('sensor_data/light');
+    mqttService.subscribeToTopic('sensor_data/pir');
+    mqttService.subscribeToTopic('segureye/control/grabar');
+    mqttService.subscribeToTopic('segureye/control/reproducir');
+    mqttService.subscribeToTopic('segureye/buzzer/control');
+
+    mqttService.client.updates!
+        .listen((List<MqttReceivedMessage<MqttMessage>> events) {
+      final MqttPublishMessage recMessage =
+          events[0].payload as MqttPublishMessage;
+      final payload =
+          MqttPublishPayload.bytesToStringAsString(recMessage.payload.message);
+      final topic = events[0].topic;
+
+      if (topic == 'sensor_data/light') {
+        setState(() {
+          _luzValue = payload;
+        });
+      } else if (topic == 'sensor_data/pir') {
+        setState(() {
+          _pirValue = payload == '1' ? '¡Movimiento Detectado!' : 'Nada';
+        });
+      } else if (topic == 'segureye/control/grabar') {
+        setState(() {
+          _microfonoValue = payload;
+        });
+      } else if (topic == 'segureye/control/reproducir') {
+        setState(() {
+          reproduccion = payload;
+        });
+      } else if (topic == 'segureye/buzzer/control') {
+        setState(() {
+          _ledValue = payload; // Ajuste el valor del LED si es necesario
+        });
+      }
+    });
   }
 
-  void _setupMQTT() {
-    mqttService.connect().then((_) {
-      // Suscribirse a tópicos de sensor de luz, PIR, y LED
-      mqttService.subscribeToTopic('sistema_segureye_esp2/sensor_luz');
-      mqttService.subscribeToTopic('sistema_segureye_esp2/sensor_pir');
-      mqttService.subscribeToTopic('sistema_segureye_esp2/led');
+  void _handlePlayback() {
+    if (_isPlaying) {
+      mqttService.publishMessage('segureye/control/reproducir', 'stop');
+      _playbackTimer?.cancel();
+      setState(() {
+        _isPlaying = false;
+      });
+    } else {
+      mqttService.publishMessage('segureye/control/reproducir', 'start');
+      setState(() {
+        _isPlaying = true;
+      });
 
-      // Escuchar actualizaciones de los tópicos
-      mqttService
-          .getStream()
-          .listen((List<MqttReceivedMessage<MqttMessage>> messages) {
-        final MqttPublishMessage recMessage =
-            messages[0].payload as MqttPublishMessage;
-        final String message = MqttPublishPayload.bytesToStringAsString(
-            recMessage.payload.message);
+      _playbackTimer = Timer(Duration(seconds: 10), () {
+        mqttService.publishMessage('segureye/control/reproducir', 'stop');
+        setState(() {
+          _isPlaying = false;
+        });
+      });
+    }
+  }
 
-        switch (messages[0].topic) {
-          case 'sistema_segureye_esp2/sensor_luz':
-            setState(() {
-              _luzValue = message;
-            });
-            break;
-          case 'sistema_segureye_esp2/sensor_pir':
-            setState(() {
-              _pirValue = message;
-            });
-            break;
-          case 'sistema_segureye_esp2/led':
-            setState(() {
-              _ledValue = message;
-            });
-            break;
-        }
+  void _handleAlarm() {
+    if (_isAlarmActive) {
+      // Si la alarma ya está activa, no hacer nada
+      return;
+    }
+    mqttService.publishMessage('segureye/buzzer/control', 'on');
+    setState(() {
+      _isAlarmActive = true;
+      _isButtonVisible = false;
+    });
+
+    // Temporizador para desactivar la alarma después de 6 segundos
+    _alarmTimer = Timer(Duration(seconds: 6), () {
+      setState(() {
+        _isAlarmActive = false;
+        _isButtonVisible = true;
       });
     });
   }
 
   @override
-  void dispose() {
-    _audioRecorder.closeRecorder();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      itemCount:
-          5, // Número de ítems a mostrar (sensor de luz, LED, micrófono, botón de reproducir, PIR)
+      itemCount: 5,
       shrinkWrap: true,
       physics: const ScrollPhysics(),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -126,25 +165,41 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
             ),
           );
         } else if (index == 1) {
-          // LED
+          // Alarma
           return CustomCard(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset('assets/icons/led.png', width: 30, height: 30),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15, bottom: 4),
-                  child: Text(
-                    _ledValue == "1" ? "Encendido" : "Apagado",
-                    style: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600),
+                if (_isButtonVisible)
+                  ElevatedButton(
+                    onPressed: _handleAlarm,
+                    child: Text("Activar Alarma"),
                   ),
-                ),
+                if (_isAlarmActive)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 4),
+                    child: Text(
+                      "Sonando",
+                      style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                if (!_isAlarmActive)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 4),
+                    child: Text(
+                      "Alarma",
+                      style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
                 const Text(
-                  "LED Status",
+                  "Alarma",
                   style: TextStyle(
                       fontSize: 13,
                       color: Colors.grey,
@@ -154,38 +209,26 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
             ),
           );
         } else if (index == 2) {
-          // Micrófono (Grabación de audio)
+          // Micrófono (Botón de grabación de audio)
           return CustomCard(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 GestureDetector(
-                  onLongPress: () async {
-                    if (!_isRecording) {
-                      setState(() {
-                        _isRecording = true;
-                      });
-                      await _audioRecorder.startRecorder();
-                      mqttService.publishMessage(
-                          'segureye/control/grabar', '{"msg": "on"}');
-                    }
+                  onLongPress: () {
+                    setState(() {
+                      _isRecording = true;
+                    });
+                    mqttService.publishMessage(
+                        'segureye/control/grabar', 'start');
                   },
-                  onLongPressUp: () async {
-                    if (_isRecording) {
-                      String? path = await _audioRecorder.stopRecorder();
-                      setState(() {
-                        _isRecording = false;
-                      });
-                      if (path != null) {
-                        final file = File(path);
-                        final fileBytes = await file.readAsBytes();
-                        mqttService.publishMessage(
-                            'sensor/microfono', fileBytes.toString());
-                      }
-                      mqttService.publishMessage(
-                          'segureye/control/grabar', '{"msg": "off"}');
-                    }
+                  onLongPressUp: () {
+                    setState(() {
+                      _isRecording = false;
+                    });
+                    mqttService.publishMessage(
+                        'segureye/control/grabar', 'stop');
                   },
                   child: Container(
                     padding: const EdgeInsets.all(5),
@@ -194,7 +237,7 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
                       children: [
                         Icon(_isRecording ? Icons.stop : Icons.mic,
                             color: Colors.white, size: 30),
-                        SizedBox(height: 10),
+                        const SizedBox(height: 10),
                         Text(
                           _isRecording ? "Grabando..." : "Grabar Audio",
                           style: const TextStyle(
@@ -224,11 +267,12 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 IconButton(
-                  icon: Icon(Icons.play_arrow, color: Colors.white, size: 30),
-                  onPressed: () {
-                    mqttService.publishMessage(
-                        'segureye/control/reproducir', '{"msg": "on"}');
-                  },
+                  icon: Icon(
+                    _isPlaying ? Icons.stop : Icons.play_arrow,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  onPressed: _handlePlayback,
                 ),
                 const Text(
                   "Reproducir Audio",
@@ -247,11 +291,17 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset('assets/icons/pir.png', width: 30, height: 30),
+                Icon(
+                  _pirValue == '¡Movimiento Detectado!'
+                      ? Icons.directions_run
+                      : Icons.remove_red_eye,
+                  color: Colors.white,
+                  size: 30,
+                ),
                 Padding(
                   padding: const EdgeInsets.only(top: 15, bottom: 4),
                   child: Text(
-                    _pirValue == "1" ? "Objeto detectado" : "Nada",
+                    _pirValue,
                     style: const TextStyle(
                         fontSize: 18,
                         color: Colors.white,
@@ -259,7 +309,7 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
                   ),
                 ),
                 const Text(
-                  "PIR Sensor",
+                  "Sensor PIR",
                   style: TextStyle(
                       fontSize: 13,
                       color: Colors.grey,
@@ -269,35 +319,16 @@ class _ActivityDetailsCardState extends State<ActivityDetailsCard> {
             ),
           );
         } else {
-          // Otros ítems
-          return CustomCard(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/icons/default.png', width: 30, height: 30),
-                const Padding(
-                  padding: EdgeInsets.only(top: 15, bottom: 4),
-                  child: Text(
-                    "N/A",
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-                const Text(
-                  "Not Available",
-                  style: TextStyle(
-                      fontSize: 13,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.normal),
-                ),
-              ],
-            ),
-          );
+          return Container(); // Just in case of an invalid index
         }
       },
     );
+  }
+
+  @override
+  void dispose() {
+    _playbackTimer?.cancel();
+    _alarmTimer?.cancel();
+    super.dispose();
   }
 }
